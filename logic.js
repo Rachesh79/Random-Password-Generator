2c507dc4d89aa27f432b6a9d3b261de69672202f
Rachesh <racheshpatil743@gmail.com>
2023-12-14 15:27:11 +0530

    fixed copying issue

--- logic.js
+++ logic.js
@@ -1,31 +1,31 @@
 
-        const passwordBox = document.getElementById("password")
-        let length = 12
+const passwordBox = document.getElementById("password")
+let length = 12
 
-        const alphabetsHigher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        const alphabetsLower = "abcdefghijklmnopqrstuvwxyxz"
-        const numbers = "0123456789"
-        const symbols = "~!@#$%^&*()_+/*\-"
+const alphabetsHigher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const alphabetsLower = "abcdefghijklmnopqrstuvwxyxz"
+const numbers = "0123456789"
+const symbols = "~!@#$%^&*()_+/*\-"
 
-        const allchars = alphabetsHigher+alphabetsLower+numbers+symbols
+const allchars = alphabetsHigher + alphabetsLower + numbers + symbols
 
-        function Gpassword(){
-            let password = ""
-            password += alphabetsHigher[Math.floor(Math.random()*alphabetsHigher.length)]
-            password += alphabetsLower[Math.floor(Math.random()*alphabetsLower.length)]
-            password += numbers[Math.floor(Math.random()*numbers.length)]
-            password += symbols[Math.floor(Math.random()*symbols.length)]
+function Gpassword() {
+    let password = ""
+    password += alphabetsHigher[Math.floor(Math.random() * alphabetsHigher.length)]
+    password += alphabetsLower[Math.floor(Math.random() * alphabetsLower.length)]
+    password += numbers[Math.floor(Math.random() * numbers.length)]
+    password += symbols[Math.floor(Math.random() * symbols.length)]
 
-            while(length > password.length){
-                password += allchars[Math.floor(Math.random()*allchars.length)]
-            }
+    while (length > password.length) {
+        password += allchars[Math.floor(Math.random() * allchars.length)]
+    }
 
-            passwordBox.value = password
-        }
+    passwordBox.value = password
+}
 
-        function CopyPassword(){
-            passwordBox.select()
-            document.execCommand('copy')
-        }
-
-        
+function CopyPassword() {
+    var passwordBox = document.getElementById("passwordBox");
+    passwordBox.select();
+    passwordBox.setSelectionRange(0,12)
+    navigator.clipboard.writeText(passwordBox.value)
+}
